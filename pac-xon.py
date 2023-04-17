136c03d0d6d570f7ef7483ae775a9a2bf8bdef12
HKustosz <92714209+HKustosz@users.noreply.github.com>
2023-04-17 10:02:28 +0200

    Base for pac-xon -  map and player model

---
+++ pac-xon.py
@@ -0,0 +1,63 @@
+import pygame
+
+pygame.init()
+
+WIDTH = 900
+HEIGHT = 700
+x_tiles = 44
+y_tiles = 29
+height = ((HEIGHT - 100) // y_tiles)
+width = (WIDTH // x_tiles)
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 20)
+color = (0, 0, 255)
+player_images = []
+for i in range(1, 5):
+    player_images.append(pygame.transform.scale(pygame.image.load(f'assets/player_images/{i}.png'), (width, height)))
+player_x = 0.5 * width
+player_y = 100
+direction = 0
+counter = 0
+
+
+def draw_board():
+    for i in range(x_tiles):
+        for j in range(y_tiles):
+            if i == 0 or i == x_tiles - 1:
+                pygame.draw.rect(screen, color, pygame.Rect(i * width + 0.5 * width, j * height + 100, width - 1, height - 1))
+            elif j == 0 or j == y_tiles - 1:
+                pygame.draw.rect(screen, color, pygame.Rect(i * width + 0.5 * width, j * height + 100, width - 1, height - 1))
+
+
+def draw_player():
+    if direction == 0:
+        screen.blit(player_images[counter //5], (player_x, player_y))
+    elif direction == 1:
+        screen.blit(pygame.transform.flip(player_images[counter //5], True, False), (player_x, player_y))
+    elif direction == 2:
+        screen.blit(pygame.transform.rotate(player_images[counter //5], 90), (player_x, player_y))
+    elif direction == 3:
+        screen.blit(pygame.transform.rotate(player_images[counter //5], -90), (player_x, player_y))
+
+
+run = True
+while run:
+    timer.tick(fps)
+    if counter < 19:
+        counter += 1
+    else:
+        counter = 0
+        
+    screen.fill('black')
+    draw_board()
+    draw_player()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+
+pygame.quit()
