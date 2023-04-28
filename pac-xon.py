4a18c11eb9a98b58677385682f32ea91680f99f5
hubert <259119@student.pwr.edu.pl>
2023-04-28 08:49:21 +0200

    pink_move fix and score

--- pac-xon.py
+++ pac-xon.py
@@ -1,4 +1,5 @@
 import copy
+import threading
 import time
 import pygame
 from board import board
@@ -8,17 +9,30 @@ from scipy.ndimage import label
 
 pygame.init()
 
+# Board
 WIDTH = 900
 HEIGHT = 700
 x_tiles = 44
 y_tiles = 29
 height = ((HEIGHT - 100) // y_tiles)
 width = (WIDTH // x_tiles)
+color = (0, 0, 255)
+board = copy.deepcopy(board)
+
+# Gameplay
+lives = 3
+point_count = 0
+fill_percent = 0
+level = 0
+
+# Display
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption("Pac-xon")
+font = pygame.font.Font('freesansbold.ttf', 30)
 timer = pygame.time.Clock()
 fps = 15
-font = pygame.font.Font('freesansbold.ttf', 20)
-color = (0, 0, 255)
+
+# Pac-man
 player_images = []
 for i in range(1, 5):
     player_images.append(pygame.transform.scale(pygame.image.load(f'assets/player_images/{i}.png'), (width, height)))
@@ -26,15 +40,15 @@ player_x = 0.5 * width
 player_y = 100
 direction = 0
 counter = 0
+
+# Pink ghost
 pink_ghost_amount = 2
 pink_ghost_x = []
 pink_ghost_y = []
 pink_ghost_direction = []
 for _ in range(pink_ghost_amount):
     pink_ghost_direction.append(0)
-board = copy.deepcopy(board)
 pink_ghost = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/pink.png'), (width, height))
-lives = 5
 
 
 def draw_board():
@@ -106,7 +120,6 @@ def fill_path():
         pass
 
 
-
 def two_to_one():
     for i in range(x_tiles):
         for j in range(y_tiles):
@@ -154,6 +167,7 @@ def move_pink():
         elif pink_ghost_direction[i] == 0 and board[pink_ghost_x[i]+1][pink_ghost_y[i]-1] == 1:
             if board[pink_ghost_x[i]][pink_ghost_y[i]-1] == 1: pink_ghost_direction[i] = 1
             elif board[pink_ghost_x[i] + 1][pink_ghost_y[i]] == 1: pink_ghost_direction[i] = 3
+            else: pink_ghost_direction[i] = 2
 
         if pink_ghost_direction[i] == 1 and board[pink_ghost_x[i]+1][pink_ghost_y[i]+1] == 2:
             lose_life()
@@ -165,6 +179,7 @@ def move_pink():
         elif pink_ghost_direction[i] == 1 and board[pink_ghost_x[i]+1][pink_ghost_y[i]+1] == 1:
             if board[pink_ghost_x[i]][pink_ghost_y[i]+1] == 1: pink_ghost_direction[i] = 0
             elif board[pink_ghost_x[i] + 1][pink_ghost_y[i]] == 1: pink_ghost_direction[i] = 2
+            else: pink_ghost_direction[i] = 3
 
         if pink_ghost_direction[i] == 2 and board[pink_ghost_x[i]-1][pink_ghost_y[i]+1] == 2:
             lose_life()
@@ -176,6 +191,7 @@ def move_pink():
         elif pink_ghost_direction[i] == 2 and board[pink_ghost_x[i]-1][pink_ghost_y[i]+1] == 1:
             if board[pink_ghost_x[i]][pink_ghost_y[i]+1] == 1: pink_ghost_direction[i] = 3
             elif board[pink_ghost_x[i] - 1][pink_ghost_y[i]] == 1: pink_ghost_direction[i] = 1
+            else: pink_ghost_direction[i] = 0
 
         if pink_ghost_direction[i] == 3 and board[pink_ghost_x[i]-1][pink_ghost_y[i]-1] == 2:
             lose_life()
@@ -187,6 +203,36 @@ def move_pink():
         elif pink_ghost_direction[i] == 3 and board[pink_ghost_x[i]-1][pink_ghost_y[i]-1] == 1:
             if board[pink_ghost_x[i]][pink_ghost_y[i]-1] == 1: pink_ghost_direction[i] = 2
             elif board[pink_ghost_x[i]-1][pink_ghost_y[i]] == 1: pink_ghost_direction[i] = 0
+            else: pink_ghost_direction[i] = 1
+
+
+def move_when_keys():
+    global player_x
+    global player_y
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT] or keys[pygame.K_LEFT] or keys[pygame.K_UP] or keys[pygame.K_DOWN]:
+        player_x, player_y = move_player(player_x, player_y)
+
+
+def display_text():
+    global lives
+    global point_count
+    global fill_percent
+    text_lives = font.render('Lives: ' + str(lives), True, (255, 255, 0))
+    text_lives_rect = text_lives.get_rect()
+    text_lives_rect.center = (100, 50)
+
+    text_points = font.render('Points: ' + str(point_count), True, (255, 255, 0))
+    text_points_rect = text_lives.get_rect()
+    text_points_rect.center = (550, 50)
+
+    text_fill = font.render('Filled: ' + str(fill_percent) + "%", True, (255, 255, 0))
+    text_fill_rect = text_lives.get_rect()
+    text_fill_rect.center = (750, 50)
+
+    screen.blit(text_lives, text_lives_rect)
+    screen.blit(text_points, text_points_rect)
+    screen.blit(text_fill, text_fill_rect)
 
 
 run = True
@@ -197,11 +243,12 @@ while run:
         counter += 1
     else:
         counter = 0
-        
+
     screen.fill('black')
     draw_board()
+    display_text()
     draw_player()
-    move_pink()
+    t1 = threading.Thread(target=move_pink).start()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -219,9 +266,7 @@ while run:
             if event.key == pygame.K_DOWN:
                 direction = 3
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT] or keys[pygame.K_LEFT] or keys[pygame.K_UP] or keys[pygame.K_DOWN]:
-        player_x, player_y = move_player(player_x, player_y)
+    t2 = threading.Thread(target=move_when_keys()).start()
 
     fill_path()
     pygame.display.flip()
